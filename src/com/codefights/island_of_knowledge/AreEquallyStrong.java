fce0d2ce1efa0e8512a990cfedd0b3b2c2b8885c
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2018-07-05 22:41:00 -0300

    Islando of Knowledge: AreEquallyStrong

---
+++ src/com/codefights/island_of_knowledge/AreEquallyStrong.java
@@ -0,0 +1,15 @@
+package com.codefights.island_of_knowledge;
+
+public class AreEquallyStrong {
+
+	public static void main(String[] args) {
+		int yourLeft = 10, yourRight = 15, friendsLeft = 15, friendsRight = 9;
+		
+		areEquallyStrong(yourLeft, yourRight, friendsLeft, friendsRight);
+	}
+	
+	static boolean areEquallyStrong(int yourLeft, int yourRight, int friendsLeft, int friendsRight) {
+		return yourLeft == friendsLeft || yourLeft == friendsRight && yourRight == friendsLeft || yourRight == friendsRight;
+	}
+	
+}
