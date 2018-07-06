ad6881dd233571fd6ed8417678f02df2d8bce50e
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2018-07-05 22:43:12 -0300

    Added parenthesis to the correct result

--- src/com/codefights/island_of_knowledge/AreEquallyStrong.java
+++ src/com/codefights/island_of_knowledge/AreEquallyStrong.java
@@ -9,7 +9,7 @@ public class AreEquallyStrong {
 	}
 	
 	static boolean areEquallyStrong(int yourLeft, int yourRight, int friendsLeft, int friendsRight) {
-		return yourLeft == friendsLeft || yourLeft == friendsRight && yourRight == friendsLeft || yourRight == friendsRight;
+		return (yourLeft == friendsLeft || yourLeft == friendsRight) && (yourRight == friendsLeft || yourRight == friendsRight);
 	}
 	
 }
