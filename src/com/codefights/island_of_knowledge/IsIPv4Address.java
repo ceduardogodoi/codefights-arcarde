052aadf4c413347c0a760fbca0689e6eeaea3ca1
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2018-07-08 23:33:23 -0300

    IsIPv4Address

---
+++ src/com/codefights/island_of_knowledge/IsIPv4Address.java
@@ -0,0 +1,26 @@
+package com.codefights.island_of_knowledge;
+
+public class IsIPv4Address {
+
+	public static void main(String[] args) {
+		String inputString = "1.255.260.1";
+
+		System.out.println(isIPv4Address(inputString));
+	}
+
+	static boolean isIPv4Address(String inputString) {
+	    String[] a = inputString.split("\\.");
+	    
+	    if (a.length != 4) return false;
+			
+		for (String s : a) {
+			if (!s.matches("\\d{1,3}")) return false;
+				
+			int v = Integer.valueOf(s);
+			if (v < 0 || v > 255) return false;
+		}
+		
+		return true;
+	}
+
+}
