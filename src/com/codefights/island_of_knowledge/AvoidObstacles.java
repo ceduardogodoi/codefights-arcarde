c8fec11ab2dbbc142057b21e7318cd426a6e5d63
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2018-08-05 19:07:46 -0300

    AvoidObstacles

---
+++ src/com/codefights/island_of_knowledge/AvoidObstacles.java
@@ -0,0 +1,16 @@
+package com.codefights.island_of_knowledge;
+
+public class AvoidObstacles {
+
+	public static void main(String[] args) {
+		int[] inputArray = { 5, 3, 6, 7, 9 };
+		
+		avoidObstacles(inputArray);
+	}
+	
+	static int avoidObstacles(int[] inputArray) {
+		
+		return 0;
+	}
+	
+}
